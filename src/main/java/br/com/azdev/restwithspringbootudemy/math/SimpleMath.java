e5a9d2b8be904ecb089f837e73d4b77a9f3c7052
AvelFir <guichasemartinsgmr@hotmail.com>
2021-02-04 00:35:02 -0300

    adicionado operacoes math e reestruturado codigo

---
+++ src/main/java/br/com/azdev/restwithspringbootudemy/math/SimpleMath.java
@@ -0,0 +1,29 @@
+package br.com.azdev.restwithspringbootudemy.math;
+
+public class SimpleMath {
+    public Double sum(Double firstNumber, Double secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public Double sub(Double firstNumber, Double secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public Double multi(Double firstNumber, Double secondNumber){
+        return firstNumber * secondNumber;
+    }
+
+    public Double div(Double firstNumber, Double secondNumber){
+        return firstNumber / secondNumber;
+    }
+
+    public Double media(Double firstNumber, Double secondNumber){
+        return (firstNumber + secondNumber)/2;
+    }
+
+    public Double sqrt(Double number){
+        return Math.sqrt(number);
+    }
+
+
+}
