b5f5fdd19ca534efc06489320e1648f8886c5924
AvelFir <guichasemartinsgmr@hotmail.com>
2021-02-11 01:25:24 -0300

    added HATEOAS

--- src/main/java/br/com/azdev/restwithspringbootudemy/RestWithSpringBootUdemyApplication.java
+++ src/main/java/br/com/azdev/restwithspringbootudemy/RestWithSpringBootUdemyApplication.java
@@ -1,9 +1,14 @@
 package br.com.azdev.restwithspringbootudemy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class RestWithSpringBootUdemyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/azdev/restwithspringbootudemy/controller/PersonController.java
+++ src/main/java/br/com/azdev/restwithspringbootudemy/controller/PersonController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -19,24 +22,33 @@ public class PersonController {
 
     @GetMapping(produces = {"application/json","application/xml","application/x-yaml"})
     public List<PersonVO> findAll (){
-        return services.findAll();
+        List<PersonVO> persons = services.findAll();
+        persons.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+        return persons;
     }
 
     @GetMapping(value= "{id}",produces = {"application/json","application/xml","application/x-yaml"})
     public PersonVO findById (@PathVariable("id") Long id){
-        return services.findById(id);
+
+        PersonVO personVO = services.findById(id);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return personVO;
     }
 
     @PostMapping(consumes = {"application/json","application/xml","application/x-yaml"},
             produces = {"application/json","application/xml","application/x-yaml"})
     public PersonVO create(@RequestBody PersonVO person){
-        return services.create(person);
+         PersonVO personVO = services.create(person);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+        return personVO;
     }
 
     @PutMapping(consumes = {"application/json","application/xml","application/x-yaml"},
             produces = {"application/json","application/xml","application/x-yaml"})
     public PersonVO update(@RequestBody PersonVO person){
-        return services.create(person);
+        PersonVO personVO = services.create(person);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+        return personVO;
     }
 
     @DeleteMapping(value= "{id}")
